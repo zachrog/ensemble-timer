5e928a3307bf47383410a22007bc23c1fed1f423
zacharyrogers1 <ztrogers1@gmail.com>
2024-01-14 20:45:28 -0800

    switch to new build project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
+    'plugin:react-hooks/recommended',
   ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-  plugins: ['@typescript-eslint', 'react'],
-  rules: {},
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-};
+}
