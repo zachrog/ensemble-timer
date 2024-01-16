46914ca707963d261888e7cd2d3bb4b816ba4a4d
zacharyrogers1 <ztrogers1@gmail.com>
2024-01-15 22:31:10 -0800

    Add settings for transparency

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 function App() {
   return (
     <>
-    <div className="h-full bg-purple-700 h-">
-      <h1>Vite + React</h1>
-      <div className="bg-green-300">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="">
     </div>
     </>
   );
